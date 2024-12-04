6dacb217977609aa81dded427a64dd6918978250
Tiago Go Enablers <tiago@goenablers.io>
2024-12-04 17:41:06 -0300

    Ajuste mensagem inicial

--- main.py
+++ main.py
@@ -40,9 +40,9 @@ async def handle_incoming_call(request: Request):
     """Handle incoming call and return TwiML response to connect to Media Stream."""
     response = VoiceResponse()
     # <Say> punctuation to improve text-to-speech flow
-    response.say("Please wait while we connect your call to the A. I. voice assistant, powered by Twilio and the Open-A.I. Realtime API")
+    response.say("Só um momento...")
     response.pause(length=1)
-    response.say("O.K. you can start talking!")
+    response.say("Oi! Alou, quem fala?")
     host = request.url.hostname
     connect = Connect()
     connect.stream(url=f'wss://{host}/media-stream')
